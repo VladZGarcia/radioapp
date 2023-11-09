9358dbe7b4f1badf6e7ab7e42dcab13ee462c67b
VladZGarcia <vladi.garcia@gmail.com>
2023-11-09 20:20:42 +0100

    able to change channels

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -1,15 +1,14 @@
-import 'package:flutter/material.dart';
 
 class CategoryModel {
   String name;
   String iconPath;
-  Color boxColor;
+  String radioUrl;
   String scheduleurl;
 
   CategoryModel(
       {required this.name,
       required this.iconPath,
-      required this.boxColor,
+      required this.radioUrl,
       required this.scheduleurl
       });
 
@@ -20,32 +19,32 @@ class CategoryModel {
       name: 'P1',
       iconPath:
           'https://static-cdn.sr.se/images/132/2186745_512_512.jpg?preset=api-default-square',
-      boxColor: const Color(0xff92A3FD),
-      scheduleurl: 'https://api.sr.se/v2/scheduledepisodes?channelid=132'
+      radioUrl: 'https://sverigesradio.se/topsy/direkt/srapi/132.mp3',
+      scheduleurl: 'https://api.sr.se/v2/scheduledepisodes?channelid=132&format=json&size=100'
     ));
 
     categories.add(CategoryModel(
       name: 'P2',
       iconPath:
           'https://static-cdn.sr.se/images/163/2186754_512_512.jpg?preset=api-default-square',
-      boxColor: const Color.fromARGB(255, 253, 146, 239),
-      scheduleurl: 'https://api.sr.se/v2/scheduledepisodes?channelid=163'
+      radioUrl: 'https://sverigesradio.se/topsy/direkt/srapi/163.mp3',
+      scheduleurl: 'https://api.sr.se/v2/scheduledepisodes?channelid=163&format=json&size=100'
     ));
 
     categories.add(CategoryModel(
       name: 'P3',
       iconPath:
           'https://static-cdn.sr.se/images/164/2186756_512_512.jpg?preset=api-default-square',
-      boxColor: const Color(0xff92A3FD),
-      scheduleurl: 'https://api.sr.se/v2/scheduledepisodes?channelid=164'
+      radioUrl: 'https://sverigesradio.se/topsy/direkt/srapi/164.mp3',
+      scheduleurl: 'https://api.sr.se/v2/scheduledepisodes?channelid=164&format=json&size=100'
     ));
 
     categories.add(CategoryModel(
       name: 'P4',
       iconPath:
           'https://static-cdn.sr.se/images/103/93751e41-25be-443e-aa50-3d65b67ae5ac.jpg?preset=api-default-square',
-      boxColor: const Color.fromARGB(255, 199, 226, 22),
-      scheduleurl: 'https://api.sr.se/v2/scheduledepisodes?channelid=701'
+      radioUrl: 'https://sverigesradio.se/topsy/direkt/srapi/701.mp3',
+      scheduleurl: 'https://api.sr.se/v2/scheduledepisodes?channelid=701&format=json&size=100'
     ));
 
     return categories;
